3c7dd76b884fb688d1079bff15f8336af8fb166c
tomaszmaslon <tomasz.maslon@gmail.com>
2022-05-20 15:02:18 +0200

    Initial commit

---
+++ app/src/main/java/com/tmaslon/plainolnotes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tmaslon.plainolnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
