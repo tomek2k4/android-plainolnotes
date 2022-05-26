e6bbb16318f32bb52d5e067fc3399241e69ce248
tomaszmaslon <tomasz.maslon@gmail.com>
2022-05-26 12:06:34 +0200

    Added jetpack navigation controller

---
+++ app/src/main/java/com/tmaslon/plainolnotes/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.tmaslon.plainolnotes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+}
